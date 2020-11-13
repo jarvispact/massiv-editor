2ef630ac385c3552c155ff77bbc61336fe193966
jarvispact <jarvispact@gmail.com>
2020-11-13 21:41:35 +0100

    chore: project setup

---
+++ webpack.config.ts
@@ -0,0 +1,38 @@
+import path from "path";
+import webpack from "webpack";
+
+const config: webpack.Configuration = {
+    entry: "./src/index.tsx",
+    module: {
+        rules: [
+          {
+              test: /\.(ts|js)x?$/,
+              exclude: /node_modules/,
+              use: {
+                  loader: "babel-loader",
+                  options: {
+                      presets: [
+                          "@babel/preset-env",
+                          "@babel/preset-react",
+                          "@babel/preset-typescript",
+                      ],
+                  },
+              },
+          },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 3000,
+    },
+};
+
+export default config;
